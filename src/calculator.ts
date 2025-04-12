e985144390b4d3e93d9bd72ebf31120b1172dea9
ashishku <ashishkujoy@gmail.com>
2025-04-12 15:53:19 +0530

    Adds support for custom delimited numbers

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,12 +1,23 @@
+const customDelimitedNumRegex = /^\/\/(.+)\n([\s\S]*)$/;
+
+const extractDelimiterAndNumStr = (numberStr: string) => {
+  const match = customDelimitedNumRegex.exec(numberStr);
+  const [delimiter, str] = match === null ? [",", numberStr] : match.slice(1, 3);
+
+  return { delimiter, str };
+}
+
 const parseNumbers = (numberStr: string) => {
-  return numberStr.split("\n")
-    .flatMap(line => line.split(","))
+  const {delimiter, str} = extractDelimiterAndNumStr(numberStr);
+
+  return str.split("\n")
+    .flatMap(line => line.split(delimiter))
     .map(token => Number(token));
 }
 
 const validateNumbers = (numbers: number[]) => {
   const negativeNumbers = numbers.filter(number => number < 0);
-  if(negativeNumbers.length !== 0) {
+  if (negativeNumbers.length !== 0) {
     throw new Error(`negative numbers not allowed ${negativeNumbers.join(",")}`)
   }
 }
--- test/calculator.test.ts
+++ test/calculator.test.ts
@@ -27,3 +27,7 @@ test("sum of numbers containing single negative number", () => {
 test("sum of numbers containing multiple negative number", () => {
   expect(() => add("1,-2,3\n-4")).toThrow("negative numbers not allowed -2,-4");
 });
+
+test("sum of numbers containing custom delimiter", () => {
+  expect(add("//$\n1$2$3$4")).toBe(10);
+});
